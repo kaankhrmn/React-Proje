d9eaad1e9fe4476fa00dec9e2431b6095126bcc5
KAAN\kaank <kaankhrmn.61@gmail.com>
2025-01-22 16:29:40 +0300

    Header

---
+++ E-Commerce/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import PageContainer from './container/PageContainer';
+import Header from './components/Header.jsx'
+
+function App() {
+
+  return (
+    <>
+      <PageContainer>
+        <Header />
+      </PageContainer>
+    </>
+  )
+}
+
+export default App
---
+++ E-Commerce/src/components/Header.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+import '../css/Header.css'
+import '../App.css'
+import { CiShoppingBasket } from "react-icons/ci";
+import { CiLight } from "react-icons/ci";
+import { IoMoonSharp } from "react-icons/io5";
+
+
+function Header() {
+    const [theme, settheme] = useState(false)
+
+    const changeTheme = () => {
+        const root = document.getElementById("root")
+        settheme(!theme)
+        if (theme) {
+            root.style.backgroundColor = "black"
+            root.style.color = "white"
+        }
+        else {
+            root.style.backgroundColor = "white"
+            root.style.color = "black"
+        }
+        settheme(!theme)
+    }
+
+
+    return (
+        <>
+            <div style={{ alignItems: 'center', flexDirection: 'row', display: 'flex', justifyContent: 'space-between' }}>
+                <div className='row-flex'>
+                    <img className='logo' src='./src/images/logo.png' />
+                    <p className='icon-text'>Kaan A.Ş</p>
+                </div>
+                <div className='row-flex'>
+                    <input className='search-input' placeholder='Bir şeyler ara' type='text' />
+                    <div>
+                        {theme ? <IoMoonSharp className='icon' onClick={changeTheme} /> : <CiLight className='icon' onClick={changeTheme} />}
+                        <CiShoppingBasket className='icon' />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Header
